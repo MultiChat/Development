27b4a73a4d04fc38f54205ae100f2e8fdf772258
OllieMartin <olivermartin410@gmail.com>
2018-07-26 23:08:20 +0100

    Refactored HelpMe and Local commands

--- multichat/src/main/java/com/olivermartin410/plugins/commands/LocalCommand.java
+++ multichat/src/main/java/com/olivermartin410/plugins/commands/LocalCommand.java
@@ -8,26 +8,33 @@ import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-public class LocalCommand
-  extends Command
-{
-  public LocalCommand()
-  {
-    super("local", "multichat.chat.mode", new String[0]);
-  }
-   
-  public void execute(CommandSender sender, String[] args)
-  {
-    if ((sender instanceof ProxiedPlayer))
-    {
-      MultiChat.globalplayers.remove(((ProxiedPlayer)sender).getUniqueId());
-      MultiChat.globalplayers.put(((ProxiedPlayer)sender).getUniqueId(), Boolean.valueOf(false));
-      sender.sendMessage(new ComponentBuilder("LOCAL CHAT ENABLED").color(ChatColor.DARK_AQUA).create());
-      sender.sendMessage(new ComponentBuilder("You will only see messages from players on the same server!").color(ChatColor.AQUA).create());
-    }
-    else
-    {
-      sender.sendMessage(new ComponentBuilder("Only players can change their chat state").color(ChatColor.RED).create());
-    }
-  }
+/**
+ * Local Chat Command
+ * <p>Players can use this command to only see the chat sent from players on their current server</p>
+ * 
+ * @author Oliver Martin (Revilo410)
+ *
+ */
+public class LocalCommand extends Command {
+
+	private static String[] aliases = new String[] {};
+
+	public LocalCommand() {
+		super("local", "multichat.chat.mode", aliases);
+	}
+
+	public void execute(CommandSender sender, String[] args) {
+
+		if ((sender instanceof ProxiedPlayer)) {
+
+			MultiChat.globalplayers.remove(((ProxiedPlayer)sender).getUniqueId());
+			MultiChat.globalplayers.put(((ProxiedPlayer)sender).getUniqueId(), Boolean.valueOf(false));
+
+			sender.sendMessage(new ComponentBuilder("LOCAL CHAT ENABLED").color(ChatColor.DARK_AQUA).create());
+			sender.sendMessage(new ComponentBuilder("You will only see messages from players on the same server!").color(ChatColor.AQUA).create());
+
+		} else {
+			sender.sendMessage(new ComponentBuilder("Only players can change their chat state").color(ChatColor.RED).create());
+		}
+	}
 }
