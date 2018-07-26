2f6926b483c7ab35a7bcf5ba6688968d43960ee3
OllieMartin <olivermartin410@gmail.com>
2018-07-26 15:28:31 +0100

    Refactored and updated the display command

--- multichat/src/main/java/com/olivermartin410/plugins/commands/DisplayCommand.java
+++ multichat/src/main/java/com/olivermartin410/plugins/commands/DisplayCommand.java
@@ -1,52 +1,53 @@
 package com.olivermartin410.plugins.commands;
 
-import com.olivermartin410.plugins.ChatManipulation;
-
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
-public class DisplayCommand
-  extends Command
-{
-  public DisplayCommand()
-  {
-    super("display", "multichat.staff.display", new String[0]);
-  }
-  
-  public void execute(CommandSender sender, String[] args)
-  {
-    if (args.length < 1)
-    {
-      sender.sendMessage(new ComponentBuilder("Display a message to the entire network").color(ChatColor.DARK_AQUA).create());
-      sender.sendMessage(new ComponentBuilder("Usage /Display <Message>").color(ChatColor.AQUA).create());
-    }
-    else
-    {
-      String Message = "";
-      for (String arg : args) {
-        Message = Message + arg + " ";
-      }
-      chatMessage(Message);
-    }
-  }
-  
-  public static void chatMessage(String Message)
-  {
-    ChatManipulation chatfix = new ChatManipulation();
-    
-    String OriginalMessage = Message;
-    
-    Message = chatfix.FixFormatCodes(Message);
-    
-    String URLBIT = chatfix.getURLBIT(OriginalMessage);
-    for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
-      onlineplayer.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&', "&f" + Message)).event(new ClickEvent(ClickEvent.Action.OPEN_URL, URLBIT)).create());
-    }
-    System.out.println("\033[33m[MultiChat][Display] " + OriginalMessage);
-  }
+/**
+ * Display Command
+ * <p>Displays a message to every player connected to the BungeeCord network</p>
+ * 
+ * @author Oliver Martin (Revilo410)
+ *
+ */
+public class DisplayCommand extends Command {
+
+	private static String[] aliases = new String[] {};
+
+	public DisplayCommand() {
+		super("display", "multichat.staff.display", aliases);
+	}
+
+	public void execute(CommandSender sender, String[] args) {
+
+		if (args.length < 1) {
+
+			sender.sendMessage(new ComponentBuilder("Display a message to the entire network").color(ChatColor.DARK_AQUA).create());
+			sender.sendMessage(new ComponentBuilder("Usage /display <message>").color(ChatColor.AQUA).create());
+
+		} else {
+
+			String message = "";
+
+			for (String arg : args) {
+				message = message + arg + " ";
+			}
+
+			displayMessage(message);
+		}
+	}
+
+	public static void displayMessage(String message) {
+
+		for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
+			onlineplayer.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&', message)));
+		}
+
+		System.out.println("\033[33m[MultiChat][Display] " + message);
+	}
 }
