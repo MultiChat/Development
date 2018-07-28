d1428ae86005cb87f7858b02775e33683549015b
OllieMartin <olivermartin410@gmail.com>
2018-07-28 22:02:29 +0100

    Refactored bulletins and announcements backend

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/Announcements.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/Announcements.java
@@ -8,131 +8,122 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
 
 public class Announcements {
 
-	private static Map<String, Integer> AKey = new HashMap<String, Integer>();
-	private static Map<String, String> Announcements = new HashMap<String, String>();
-	
+	private static Map<String, Integer> aKey = new HashMap<String, Integer>();
+	private static Map<String, String> announcements = new HashMap<String, String>();
+
 	public static boolean startAnnouncement(final String name, Integer minutes) {
-			
-			if(!(AKey.containsKey(name.toLowerCase())) && Announcements.containsKey(name.toLowerCase())) {
-			
+
+		if(!(aKey.containsKey(name.toLowerCase())) && announcements.containsKey(name.toLowerCase())) {
+
 			Integer ID;
-			
-			ScheduledTask task = ProxyServer.getInstance().getScheduler().schedule(MultiChat.getInstance(), new Runnable()
-		    {
-			  @Override
-		      public void run()
-		      {
-		        String message;
-		        String URLBIT;
-		    	ChatManipulation chatman = new ChatManipulation();
-		    	 message = chatman.FixFormatCodes(Announcements.get(name.toLowerCase()));
-		    	  URLBIT = chatman.getURLBIT(Announcements.get(name.toLowerCase()));
-		    	  
-		    	  for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
-						onlineplayer.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&',message)).event(new ClickEvent(ClickEvent.Action.OPEN_URL, URLBIT)).create());
+
+			ScheduledTask task = ProxyServer.getInstance().getScheduler().schedule(MultiChat.getInstance(), new Runnable() {
+
+				@Override
+				public void run() {
+					String message = announcements.get(name.toLowerCase());
+
+					for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
+						onlineplayer.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',message)));
 					}
-		    	  
-		      }
-				
-		    }, 0, minutes, TimeUnit.MINUTES);
-			
+				}
+
+			}, 0, minutes, TimeUnit.MINUTES);
+
 			ID = task.getId();
 
-			AKey.put(name.toLowerCase(), ID);
-			
+			aKey.put(name.toLowerCase(), ID);
+
 			return true;
-			
-			} else {
-				
-				return false;
-				
-			}
-		
+
+		} else {
+
+			return false;
+
+		}
 	}
-	
+
 	public static HashMap<String,String> getAnnouncementList() {
-		
-		return (HashMap<String, String>) Announcements;
-		
+
+		return (HashMap<String, String>) announcements;
+
 	}
-	
+
 	public static void loadAnnouncementList(HashMap<String, String> loadedAnnouncements) {
-		
-		Announcements = loadedAnnouncements;
-		
+
+		announcements = loadedAnnouncements;
+
 	}
-	
+
 	public static boolean stopAnnouncement(String name) {
-		
-		if (AKey.containsKey(name.toLowerCase())) {
-			ProxyServer.getInstance().getScheduler().cancel(AKey.get(name.toLowerCase()));
-			AKey.remove(name.toLowerCase());
+
+		if (aKey.containsKey(name.toLowerCase())) {
+			ProxyServer.getInstance().getScheduler().cancel(aKey.get(name.toLowerCase()));
+			aKey.remove(name.toLowerCase());
 			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	public static boolean addAnnouncement(String name, String message) {
-		
-		if (!Announcements.containsKey(name.toLowerCase())) {
-	
-		Announcements.put(name.toLowerCase(), message);
-		
-		return true;
-		
+
+		if (!announcements.containsKey(name.toLowerCase())) {
+
+			announcements.put(name.toLowerCase(), message);
+			return true;
+
 		} else {
-			
 			return false;
-			
 		}
-		
+
 	}
-	
+
 	public static boolean removeAnnouncement(String name) {
-		
-		if (AKey.containsKey(name.toLowerCase())) {
-			ProxyServer.getInstance().getScheduler().cancel(AKey.get(name.toLowerCase()));
-			AKey.remove(name.toLowerCase());
+
+		if (aKey.containsKey(name.toLowerCase())) {
+			ProxyServer.getInstance().getScheduler().cancel(aKey.get(name.toLowerCase()));
+			aKey.remove(name.toLowerCase());
 		}
-		if(Announcements.containsKey(name.toLowerCase())) {
-			Announcements.remove(name.toLowerCase());
+
+		if(announcements.containsKey(name.toLowerCase())) {
+			announcements.remove(name.toLowerCase());
 			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	public static boolean existsAnnouncemnt(String name) {
-		
-		if ( Announcements.containsKey(name.toLowerCase() ) ) {
+
+		if ( announcements.containsKey(name.toLowerCase() ) ) {
 			return true;
 		} else {
 			return false;
 		}
-		
+
 	}
-	
+
 	public static void playAnnouncement(String name) {
-		
-		if (Announcements.containsKey(name.toLowerCase())) {
-		String message;
-        String URLBIT;
-    	ChatManipulation chatman = new ChatManipulation();
-    	 message = chatman.FixFormatCodes(Announcements.get(name.toLowerCase()));
-    	  URLBIT = chatman.getURLBIT(Announcements.get(name.toLowerCase()));
-    	  
-    	  for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
+
+		if (announcements.containsKey(name.toLowerCase())) {
+			String message;
+			String URLBIT;
+			ChatManipulation chatman = new ChatManipulation();
+			message = chatman.FixFormatCodes(announcements.get(name.toLowerCase()));
+			URLBIT = chatman.getURLBIT(announcements.get(name.toLowerCase()));
+
+			for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
 				onlineplayer.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&',message)).event(new ClickEvent(ClickEvent.Action.OPEN_URL, URLBIT)).create());
 			}
 		}
 	}
-	
 }
--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/Bulletins.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/Bulletins.java
@@ -6,8 +6,7 @@ import java.util.concurrent.TimeUnit;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.ProxyServer;
-import net.md_5.bungee.api.chat.ClickEvent;
-import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.scheduler.ScheduledTask;
 
@@ -17,29 +16,30 @@ public class Bulletins {
 	private static int nextBulletin = -1;
 	private static ArrayList<String> bulletin = new ArrayList<String>();
 	private static int timeInbetween = 0;
- 
+
 	public static void setArrayList(ArrayList<String> bulletinList) {
 		bulletin = bulletinList;
 	}
-	
+
 	public static ArrayList<String> getArrayList() {
 		return bulletin;
 	}
-	
+
 	public static void startBulletins(int timeBetween) {
 
 		timeInbetween = timeBetween;
+
 		if (nextBulletin == -1 && bulletin.size() > 0) {
 			nextBulletin = 0;
 			scheduleNextBulletin(timeBetween);
 		}
 
 	}
-	
+
 	public static int getTimeBetween() {
 		return timeInbetween;
 	}
-	
+
 	public static boolean isEnabled() {
 		if (nextBulletin == -1) { 
 			return false;
@@ -49,23 +49,25 @@ public class Bulletins {
 	}
 
 	public static void stopBulletins() {
+
 		if (nextBulletin != -1) {
 			nextBulletin = -1;
 		}
+
 		try {
 			ProxyServer.getInstance().getScheduler().cancel(currentlyScheduled);
 		} catch (Exception e) {
 			System.err.println(e);
 		}
+
 	}
 
 	public static void addBulletin(String message) {
-
 		synchronized (bulletin) {
 			bulletin.add(message);
 		}
 	}
-	
+
 	public static Iterator<String> getIterator() {
 		synchronized (bulletin) {
 			return bulletin.iterator();
@@ -75,44 +77,46 @@ public class Bulletins {
 	public static void removeBulletin(int index) {
 
 		synchronized (bulletin) {
-		try {
-			bulletin.remove(index);
-		} catch (Exception e) {
-			System.err.println("Couldnt remove bulletin!");
-		}
+			try {
+				bulletin.remove(index);
+			} catch (Exception e) {
+				System.err.println("Couldnt remove bulletin!");
+			}
 		}
 
 	}
 
 	private static void scheduleNextBulletin(final int minutes) {
-		ScheduledTask task = ProxyServer.getInstance().getScheduler().schedule(MultiChat.getInstance(), new Runnable()
-		{
+		ScheduledTask task = ProxyServer.getInstance().getScheduler().schedule(MultiChat.getInstance(), new Runnable() {
+
 			@Override
-			public void run()
-			{
+			public void run() {
+
 				String message;
-				String URLBIT;
-				ChatManipulation chatman = new ChatManipulation();
+
 				if (bulletin.size() < 1) {
+					/* EMPTY */
 				} else {
-					message = chatman.FixFormatCodes(bulletin.get(nextBulletin));
-					URLBIT = chatman.getURLBIT(bulletin.get(nextBulletin));
+
+					message = bulletin.get(nextBulletin);
 
 					for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
-						onlineplayer.sendMessage(new ComponentBuilder(ChatColor.translateAlternateColorCodes('&',message)).event(new ClickEvent(ClickEvent.Action.OPEN_URL, URLBIT)).create());
+						onlineplayer.sendMessage(TextComponent.fromLegacyText(ChatColor.translateAlternateColorCodes('&',message)));
 					}
 
 				}
+
 				if (nextBulletin >= bulletin.size() - 1) {
 					nextBulletin = 0;
 				} else {
 					nextBulletin++;
 				}
+
 				scheduleNextBulletin(minutes);
 			}
 
 		}, minutes, TimeUnit.MINUTES);
+
 		currentlyScheduled = task.getId();
 	}
-
 }
