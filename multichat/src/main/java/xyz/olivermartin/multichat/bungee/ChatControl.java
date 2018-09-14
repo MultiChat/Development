c5092dbce061f5f97a6fe2ecc4acb1aa74fb358e
OllieMartin <olivermartin410@gmail.com>
2018-09-14 23:59:10 +0100

    More work on starts of the chat control module

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/ChatControl.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/ChatControl.java
@@ -8,10 +8,14 @@ import net.md_5.bungee.config.Configuration;
 public class ChatControl {
 
 	@SuppressWarnings("rawtypes")
-	public static String applyChatRules(String input) {
+	public static String applyChatRules(String input, String chatType) {
 
 		// TODO Actually register this config file in the code etc.
 		Configuration config = ConfigManager.getInstance().getHandler("chatcontrol.yml").getConfig();
+
+		if (!config.contains("apply_rules_to." + chatType)) return input;
+		if (!config.getBoolean("apply_rules_to." + chatType)) return input;
+
 		List rules = config.getList("regex_rules");
 
 		if (rules != null) {
