f8a018998eaba1d96e6f486c9871a759385fb50b
OllieMartin <olivermartin410@gmail.com>
2019-01-31 18:44:12 +0000

    Fixed error with message manager

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/MultiChat.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/MultiChat.java
@@ -231,8 +231,8 @@ public class MultiChat extends Plugin implements Listener {
 
 		ConfigDir = getDataFolder();
 		if (!getDataFolder().exists()) {
-			//System.out.println("[MultiChat] Creating plugin directory!");
-			ConsoleManager.log("Creating plugin directory...");
+			System.out.println("[MultiChat] Creating plugin directory!");
+			//ConsoleManager.log("Creating plugin directory...");
 			getDataFolder().mkdirs();
 		}
 
@@ -282,7 +282,7 @@ public class MultiChat extends Plugin implements Listener {
 			// Register commands
 			registerCommands(configYML, chatcontrolYML);
 
-			//System.out.println("[MultiChat] Config Version: " + configversion);
+			System.out.println("[MultiChat] Config Version: " + configversion);
 			ConsoleManager.log("Config Version: " + configversion);
 
 			// Run start-up routines
