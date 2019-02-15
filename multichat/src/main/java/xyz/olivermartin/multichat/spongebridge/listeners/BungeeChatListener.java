17345ae75a74d9e55c2f67bef745914a6fe862c6
OllieMartin <olivermartin410@gmail.com>
2019-02-15 14:05:11 +0000

    Send casts to sponge for local display

--- multichat/src/main/java/xyz/olivermartin/multichat/spongebridge/listeners/BungeeChatListener.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/spongebridge/listeners/BungeeChatListener.java
@@ -1,10 +1,12 @@
 package xyz.olivermartin.multichat.spongebridge.listeners;
 
 import org.spongepowered.api.Platform.Type;
+import org.spongepowered.api.Sponge;
 import org.spongepowered.api.network.ChannelBinding.RawDataChannel;
 import org.spongepowered.api.network.ChannelBuf;
 import org.spongepowered.api.network.RawDataListener;
 import org.spongepowered.api.network.RemoteConnection;
+import org.spongepowered.api.text.serializer.TextSerializers;
 
 public class BungeeChatListener implements RawDataListener {
 
@@ -16,6 +18,9 @@ public class BungeeChatListener implements RawDataListener {
 	public void handlePayload(ChannelBuf data, RemoteConnection connection, Type side) {
 
 		// TODO Legacy, needs to be replaced with cast management
+
+		String message = data.readUTF();
+		Sponge.getServer().getBroadcastChannel().send(TextSerializers.FORMATTING_CODE.deserialize(message));
 
 		/*Optional<Player> player = Sponge.getServer().getPlayer(data.readUTF());
 
