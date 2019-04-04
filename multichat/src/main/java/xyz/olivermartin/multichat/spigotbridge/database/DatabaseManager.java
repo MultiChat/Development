f942646c22b4228b4be71e1ff14b160d7d7467c1
OllieMartin <olivermartin410@gmail.com>
2019-04-04 10:53:53 +0100

    SQLite is now a maven dependency

--- multichat/src/main/java/xyz/olivermartin/multichat/spigotbridge/database/DatabaseManager.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/spigotbridge/database/DatabaseManager.java
@@ -1,5 +1,10 @@
 package xyz.olivermartin.multichat.spigotbridge.database;
 
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseManager {
 
 	private static DatabaseManager instance;
@@ -17,5 +22,30 @@ public class DatabaseManager {
 	private DatabaseManager() {
 		/* EMPTY */
 	}
+	
+	public static void main(String args[]) {
+		
+		DatabaseManager.getInstance().createDatabase("testing.db");
+		
+	}
+	
+	/**
+	 * Generic class to create a sqlite database
+	 */
+    public void createDatabase(String fileName) {
+ 
+        String url = "jdbc:sqlite:C:/sqlite/db/" + fileName;
+ 
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+ 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 	
 }
