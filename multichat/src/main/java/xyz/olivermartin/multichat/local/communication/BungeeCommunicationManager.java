1b2dcf9cb298f597eb49f948fa009d135b912a04
OllieMartin <olivermartin410@gmail.com>
2020-04-09 21:56:28 +0100

    Added comment

--- multichat/src/main/java/xyz/olivermartin/multichat/local/communication/BungeeCommunicationManager.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/local/communication/BungeeCommunicationManager.java
@@ -3,5 +3,7 @@ package xyz.olivermartin.multichat.local.communication;
 public class BungeeCommunicationManager extends ProxyCommunicationManager {
 
 // Not sure how best to do this... will need a SpigotBungeeCommunicationManager and SpongeBungeeCommunicationManager??????
+	
+	// It might be best this shouldnt be part of the API at all? Considering its the kind of thing that should actually be USING the API...
 
 }
