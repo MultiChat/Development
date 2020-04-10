df02621b61c2690c1308f7ac92b9b3128324cd65
OllieMartin <olivermartin410@gmail.com>
2020-04-10 13:53:46 +0100

    Added all the main classes from spigot and sponge main package to local

---
+++ multichat/src/main/java/xyz/olivermartin/multichat/local/LocalPseudoChannel.java
@@ -0,0 +1,20 @@
+package xyz.olivermartin.multichat.local;
+
+import java.util.List;
+import java.util.UUID;
+
+public class LocalPseudoChannel {
+
+	public LocalPseudoChannel(String name, List<UUID> members, boolean whitelist) {
+
+		this.name = name;
+		this.whitelistMembers = whitelist;
+		this.members = members;
+
+	}
+
+	public String name;
+	public boolean whitelistMembers;
+	public  List<UUID> members;
+
+}
