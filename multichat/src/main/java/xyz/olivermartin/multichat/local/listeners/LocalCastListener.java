dc890d0513fc396fcb4bd3dbce5bfaf8f6cf8caf
OllieMartin <olivermartin410@gmail.com>
2020-04-13 15:37:12 +0100

    Implemented local cast listener

---
+++ multichat/src/main/java/xyz/olivermartin/multichat/local/listeners/LocalCastListener.java
@@ -0,0 +1,28 @@
+package xyz.olivermartin.multichat.local.listeners;
+
+import java.io.IOException;
+
+import xyz.olivermartin.multichat.local.MultiChatLocal;
+
+public abstract class LocalCastListener {
+
+	protected boolean handleMessage(LocalBungeeMessage message) {
+
+		try {
+
+			String castMessage = message.readUTF();
+			broadcastRawMessageToChat(castMessage);
+			return true;
+
+		} catch (IOException e) {
+
+			MultiChatLocal.getInstance().getConsoleLogger().log("An error occurred trying to read local cast message from Bungeecord, is the server lagging?");
+			return false;
+
+		}
+
+	}
+
+	protected abstract void broadcastRawMessageToChat(String message);
+
+}
