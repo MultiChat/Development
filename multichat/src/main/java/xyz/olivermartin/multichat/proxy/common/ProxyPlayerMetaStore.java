38b099960d0050be55e53527941d5b831d38896a
OllieMartin <olivermartin410@gmail.com>
2020-04-20 17:55:39 +0100

    Implemented abstract player meta store

--- multichat/src/main/java/xyz/olivermartin/multichat/proxy/common/ProxyPlayerMetaStore.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/proxy/common/ProxyPlayerMetaStore.java
@@ -22,11 +22,27 @@ public abstract class ProxyPlayerMetaStore {
 	public abstract String getPrefix(UUID uuid);
 
 	/**
+	 * Offer a value for the prefix for this player
+	 * Note, if the offer will be used or discarded is dependent on the MODE of the PlayerMetaStore
+	 * @param uuid
+	 * @param prefix
+	 */
+	public abstract void offerPrefix(UUID uuid, String prefix);
+
+	/**
 	 * Get the suffix of an online player
 	 * @param uuid
 	 * @return The suffix if they are online, or blank if they are not
 	 */
 	public abstract String getSuffix(UUID uuid);
+
+	/**
+	 * Offer a value for the suffix for this player
+	 * Note, if the offer will be used or discarded is dependent on the MODE of the PlayerMetaStore
+	 * @param uuid
+	 * @param suffix
+	 */
+	public abstract void offerSuffix(UUID uuid, String suffix);
 
 	/**
 	 * Get the world of an online player
@@ -36,11 +52,27 @@ public abstract class ProxyPlayerMetaStore {
 	public abstract String getWorld(UUID uuid);
 
 	/**
+	 * Offer a value for the world for this player
+	 * Note, if the offer will be used or discarded is dependent on the MODE of the PlayerMetaStore
+	 * @param uuid
+	 * @param world
+	 */
+	public abstract void offerWorld(UUID uuid, String world);
+
+	/**
 	 * Gets the nickname if one is set, otherwise gets the username
 	 * @param uuid
 	 * @return The nickname of the player if they are online, blank if they are not
 	 */
 	public abstract String getCurrentName(UUID uuid);
+
+	/**
+	 * Offer a value for the current name for this player
+	 * Note, if the offer will be used or discarded is dependent on the MODE of the PlayerMetaStore
+	 * @param uuid
+	 * @param currentName
+	 */
+	public abstract void offerCurrentName(UUID uuid, String currentName);
 
 	/**
 	 * Get the name of an online player
@@ -50,10 +82,26 @@ public abstract class ProxyPlayerMetaStore {
 	public abstract String getName(UUID uuid);
 
 	/**
-	 * Get the displayname of an online player
+	 * Offer a value for the name for this player
+	 * Note, if the offer will be used or discarded is dependent on the MODE of the PlayerMetaStore
 	 * @param uuid
-	 * @return The displayname if they are online, or blank if they are not
+	 * @param name
+	 */
+	public abstract void offerName(UUID uuid, String name);
+
+	/**
+	 * Get the display name of an online player
+	 * @param uuid
+	 * @return The display name if they are online, or blank if they are not
 	 */
 	public abstract String getDisplayName(UUID uuid);
+
+	/**
+	 * Offer a value for the display name for this player
+	 * Note, if the offer will be used or discarded is dependent on the MODE of the PlayerMetaStore
+	 * @param uuid
+	 * @param displayname
+	 */
+	public abstract void offerDisplayName(UUID uuid, String displayName);
 
 }
