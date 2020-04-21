01032259e5968347a9e32e1067e2a7a4a78f123b
OllieMartin <olivermartin410@gmail.com>
2020-04-21 14:22:01 +0100

    Finished implementing join messages config

--- multichat/src/main/java/xyz/olivermartin/multichat/proxy/common/config/ProxyJoinMessagesConfig.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/proxy/common/config/ProxyJoinMessagesConfig.java
@@ -6,7 +6,20 @@ import xyz.olivermartin.multichat.proxy.common.MultiChatProxyPlatform;
 
 public abstract class ProxyJoinMessagesConfig extends ProxyConfig {
 
-	// TODO ADD ATTRIBUTES
+	// Join Messages
+	private boolean showJoinMessages;
+	private boolean showQuitMessages;
+
+	private String serverJoinFormat;
+	private String silentJoinFormat;
+	private String serverQuitFormat;
+	private String silentQuitFormat;
+
+	// Welcome messages
+	private boolean welcome;
+	private String welcomeMessage;
+	private boolean privateWelcome;
+	private String privateWelcomeMessage;
 
 	public ProxyJoinMessagesConfig(File configPath, String fileName, MultiChatProxyPlatform platform) {
 		super(configPath, fileName, platform);
@@ -15,17 +28,91 @@ public abstract class ProxyJoinMessagesConfig extends ProxyConfig {
 	@Override
 	protected void setMemberAttributes() {
 
-		// TODO E.g
+		// Join Messages
+		showJoinMessages = getBoolean("showjoin", true);
+		showQuitMessages = getBoolean("showquit", true);
 
-		// GENERAL
-		//fetchSpigotDisplayNames = getBoolean("fetch_spigot_display_names", true);
-		//setDisplayName = getBoolean("set_display_name", true);
-		//displayNameFormat = getString("display_name_format", "%PREFIX%%NICK%%SUFFIX%");
+		serverJoinFormat = getString("serverjoin", "&e%NAME% &ejoined the network");
+		silentJoinFormat = getString("silentjoin", "&b&o%NAME% &b&ojoined the network silently");
+		serverQuitFormat = getString("networkquit", "&e%NAME% left the network");
+		silentQuitFormat = getString("silentquit", "&b&o%NAME% &b&oleft the network silently");
 
-		// PMs
-		//usePrivateMessaging = getBoolean("pm", true);
-		//noPrivateMessaging = getStringList("no_pm");
+		// Welcome messages
+		welcome = getBoolean("welcome", true);
+		welcomeMessage = getString("welcome_message", "&dWelcome %NAME% to the network for the first time!");
+		privateWelcome = getBoolean("private_welcome", false);
+		privateWelcomeMessage = getString("private_welcome_message", "&5Hi there %NAME%, please make sure you read the /rules!");
 
+	}
+
+	/**
+	 * @return the showJoinMessages
+	 */
+	public boolean isShowJoinMessages() {
+		return showJoinMessages;
+	}
+
+	/**
+	 * @return the showQuitMessages
+	 */
+	public boolean isShowQuitMessages() {
+		return showQuitMessages;
+	}
+
+	/**
+	 * @return the serverJoinFormat
+	 */
+	public String getServerJoinFormat() {
+		return serverJoinFormat;
+	}
+
+	/**
+	 * @return the silentJoinFormat
+	 */
+	public String getSilentJoinFormat() {
+		return silentJoinFormat;
+	}
+
+	/**
+	 * @return the serverQuitFormat
+	 */
+	public String getServerQuitFormat() {
+		return serverQuitFormat;
+	}
+
+	/**
+	 * @return the silentQuitFormat
+	 */
+	public String getSilentQuitFormat() {
+		return silentQuitFormat;
+	}
+
+	/**
+	 * @return the welcome
+	 */
+	public boolean isWelcome() {
+		return welcome;
+	}
+
+	/**
+	 * @return the welcomeMessage
+	 */
+	public String getWelcomeMessage() {
+		return welcomeMessage;
+	}
+
+	/**
+	 * @return the privateWelcome
+	 */
+	public boolean isPrivateWelcome() {
+		return privateWelcome;
+	}
+
+	/**
+	 * @return the privateWelcomeMessage
+	 */
+	public String getPrivateWelcomeMessage() {
+		return privateWelcomeMessage;
 	}
 
 	// TODO Add Getters and Setters
