f754b2f49f6e804e89a08a0693dc5498a3223aad
OllieMartin <olivermartin410@gmail.com>
2020-04-23 13:39:08 +0100

    Added the MultiChatUtil class

---
+++ multichat/src/main/java/xyz/olivermartin/multichat/common/MultiChatUtil.java
@@ -0,0 +1,71 @@
+package xyz.olivermartin.multichat.common;
+
+import java.util.Collection;
+
+public class MultiChatUtil {
+
+	/**
+	 * Concatenate the arguments together to get the message as a string
+	 * 
+	 * @param args The arguments of the command
+	 * @param start The (zero-indexed) starting index of the message (inclusive)
+	 * @param end The (zero-indexed) finishing index of the message (inclusive)
+	 * @return The concatenated message
+	 */
+	public static String getMessageFromArgs(String[] args, int start, int end) {
+
+		int counter = 0;
+		String message = "";
+		for (String arg : args) {
+			if (counter >= start && counter <= end) {
+				message = message + arg + " ";
+			}
+			counter++;
+		}
+
+		return message;
+
+	}
+
+	/**
+	 * Concatenate the arguments together to get the message as a string
+	 * 
+	 * @param args The arguments of the command
+	 * @param start The (zero-indexed) starting index of the message (inclusive)
+	 * @return The concatenated message
+	 */
+	public static String getMessageFromArgs(String[] args, int start) {
+
+		return getMessageFromArgs(args, start, args.length - 1);
+
+	}
+
+	/**
+	 * Concatenate the arguments together to get the message as a string
+	 * 
+	 * @param args The arguments of the command
+	 * @return The concatenated message
+	 */
+	public static String getMessageFromArgs(String[] args) {
+
+		return getMessageFromArgs(args, 0, args.length - 1);
+
+	}
+
+	public static String getStringFromCollection(Collection<String> collection) {
+
+		String result = "";
+
+		for (String item : collection) {
+			if (result.equals("")) {
+				result = result + item;
+			} else {
+				result = result + " " + item;
+			}
+		}
+
+		return result;
+
+	}
+
+}
