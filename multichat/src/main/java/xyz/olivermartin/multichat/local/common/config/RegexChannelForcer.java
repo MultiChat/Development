852d34b11034beb9067b3415dea73e67034c499a
OllieMartin <olivermartin410@gmail.com>
2020-04-27 19:11:49 +0100

    Attempted to add RegexChannelForcers to the local configs

---
+++ multichat/src/main/java/xyz/olivermartin/multichat/local/common/config/RegexChannelForcer.java
@@ -0,0 +1,42 @@
+package xyz.olivermartin.multichat.local.common.config;
+
+import xyz.olivermartin.multichat.local.common.MultiChatLocal;
+
+public class RegexChannelForcer {
+
+	private String regex;
+	private boolean ignoreFormatCodes;
+	private String channel;
+
+	public RegexChannelForcer(String regex, boolean ignoreFormatCodes, String channel) {
+
+		this.regex = regex;
+		this.ignoreFormatCodes = ignoreFormatCodes;
+		this.channel = channel;
+
+		// Do not allow any other channel... YET...
+		if (! (this.channel.equalsIgnoreCase("local")
+				|| this.channel.equalsIgnoreCase("global")
+				|| this.channel.equalsIgnoreCase("current")) ) {
+			this.channel = "current";
+		}
+
+	}
+
+	public boolean matchesRegex(String messageFormat) {
+
+		String testMessage = messageFormat;
+
+		if (ignoreFormatCodes) {
+			testMessage = MultiChatLocal.getInstance().getNameManager().stripAllFormattingCodes(testMessage);
+		}
+
+		return testMessage.matches(regex);
+
+	}
+
+	public String getChannel() {
+		return this.channel;
+	}
+
+}
--- multichat/src/main/java/xyz/olivermartin/multichat/local/sponge/LocalSpongeConfig.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/local/sponge/LocalSpongeConfig.java
@@ -2,16 +2,22 @@ package xyz.olivermartin.multichat.local.sponge;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.google.common.reflect.TypeToken;
+
 import ninja.leaping.configurate.ConfigurationNode;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
+import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import ninja.leaping.configurate.yaml.YAMLConfigurationLoader;
+import xyz.olivermartin.multichat.local.common.MultiChatLocal;
 import xyz.olivermartin.multichat.local.common.MultiChatLocalPlatform;
 import xyz.olivermartin.multichat.local.common.config.LocalConfig;
 import xyz.olivermartin.multichat.local.common.config.LocalConfigStatus;
+import xyz.olivermartin.multichat.local.common.config.RegexChannelForcer;
 
 public class LocalSpongeConfig extends LocalConfig {
 
@@ -60,6 +66,46 @@ public class LocalSpongeConfig extends LocalConfig {
 	protected Map<String, String> getPlaceholdersMap(String rootConfigNode) {
 		Map<String,String> map = new HashMap<String,String>();
 		return map; // TODO This feature is currently unused in Sponge.
+	}
+
+	@SuppressWarnings({ "serial", "rawtypes" })
+	@Override
+	protected List<RegexChannelForcer> getRegexChannelForcers(String rootConfigNode) {
+
+		List<RegexChannelForcer> regexChannelForcers = new ArrayList<RegexChannelForcer>();
+
+		if (!config.getNode(rootConfigNode).isVirtual()) {
+
+			List configData;
+
+			try {
+
+				configData = config.getNode(rootConfigNode).getList(new TypeToken<Map>() { /* EMPTY */ });
+
+				for (Object configItem : configData) {
+
+					Map dictionary = (Map) configItem;
+
+					String regex = String.valueOf(dictionary.get("regex"));
+					boolean ignoreFormatCodes = (Boolean)(dictionary.get("ignore_format_codes"));
+					String channel = String.valueOf(dictionary.get("channel"));
+
+					RegexChannelForcer regexChannelForcer = new RegexChannelForcer(regex, ignoreFormatCodes, channel);
+					regexChannelForcers.add(regexChannelForcer);
+
+				}
+
+			} catch (ObjectMappingException e) {
+
+				/* EMPTY */
+				MultiChatLocal.getInstance().getConsoleLogger().debug("Could not load RegexChannelForcers...");
+
+			}
+
+		}
+
+		return regexChannelForcers;
+
 	}
 
 }
