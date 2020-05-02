9c8e531b3e0ccdd2ab3de5af5483c80092e3d81c
OllieMartin <olivermartin410@gmail.com>
2020-05-02 13:50:11 +0100

    Updated SpongeAPI to 7.2.0, Vault to 1.7, and plugin version to 2.0

--- multichat/src/main/java/xyz/olivermartin/multichat/common/MultiChatInfo.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/common/MultiChatInfo.java
@@ -2,6 +2,6 @@ package xyz.olivermartin.multichat.common;
 
 public class MultiChatInfo {
 
-	public static final String LATEST_VERSION = "1.9";
+	public static final String LATEST_VERSION = "2.0";
 
 }
