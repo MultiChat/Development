1811082de24382f35fc4a316cfa523f497ec878f
OllieMartin <olivermartin410@gmail.com>
2020-07-06 22:45:58 +0100

    Increment version number

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/MultiChat.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/MultiChat.java
@@ -37,11 +37,12 @@ import net.md_5.bungee.event.EventHandler;
  */
 public class MultiChat extends Plugin implements Listener {
 
-	public static final String LATEST_VERSION = "1.9.2";
+	public static final String LATEST_VERSION = "1.9.3";
 
 	public static final String[] ALLOWED_VERSIONS = new String[] {
 
 			LATEST_VERSION,
+			"1.9.2",
 			"1.9.1",
 			"1.9",
 			"1.8.2",
