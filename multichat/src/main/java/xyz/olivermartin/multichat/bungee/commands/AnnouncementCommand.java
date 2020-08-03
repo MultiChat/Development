0d28061af7f6cda8c873e45193d1dc57a7a9f1ac
Gadse <lilahippie@gmail.com>
2020-08-03 22:42:17 +0200

    AnnouncementCommand overhaul

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/AnnouncementCommand.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/AnnouncementCommand.java
@@ -1,7 +1,6 @@
 package xyz.olivermartin.multichat.bungee.commands;
 
-import java.util.Iterator;
-import java.util.Map;
+import java.util.Arrays;
 
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
@@ -10,158 +9,123 @@ import net.md_5.bungee.api.plugin.Command;
 import xyz.olivermartin.multichat.bungee.Announcements;
 import xyz.olivermartin.multichat.bungee.ConfigManager;
 import xyz.olivermartin.multichat.bungee.MessageManager;
-import xyz.olivermartin.multichat.common.MultiChatUtil;
 import xyz.olivermartin.multichat.proxy.common.config.ConfigFile;
 
 /**
  * Announcement Command
  * <p>Allows the user to create, remove or use announcements</p>
- * 
- * @author Oliver Martin (Revilo410)
  *
+ * @author Oliver Martin (Revilo410)
  */
 public class AnnouncementCommand extends Command {
 
-	public AnnouncementCommand() {
-		super("mcannouncement", "multichat.announce", (String[]) ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("announcement").toArray(new String[0]));
-	}
+    public AnnouncementCommand() {
+        super("mcannouncement", "multichat.announce", ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("announcement").toArray(new String[0]));
+    }
 
-	public void execute(CommandSender sender, String[] args) {
-
-		if (args.length < 1) {
-
-			showCommandUsage(sender);
-
-		} else if (args.length == 1) {
-
-			if (args[0].toLowerCase().equals("list")) {
-
-				Map<String,String> announcementList = Announcements.getAnnouncementList();
-				Iterator<String> it = announcementList.keySet().iterator();
-
-				MessageManager.sendMessage(sender, "command_announcement_list");
-
-				String currentItem;
-				while (it.hasNext()) {
-					currentItem = it.next();
-					MessageManager.sendSpecialMessage(sender, "command_announcement_list_item", currentItem + ": +++" + announcementList.get(currentItem), true);
-				}
-
-			} else if (Announcements.existsAnnouncemnt(args[0].toLowerCase())) {
-
-				Announcements.playAnnouncement(args[0].toLowerCase());
-
-			} else {
-
-				MessageManager.sendSpecialMessage(sender, "command_announcement_does_not_exist", args[0].toUpperCase());
-
-			}
-
-		} else if (args.length == 2) {
-
-			if (args[0].toLowerCase().equals("remove")) {
-
-				if (Announcements.removeAnnouncement(args[1].toLowerCase()) == true) {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_removed", args[1].toUpperCase());
-				} else {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_does_not_exist", args[1].toUpperCase());
-				}
-
-			} else if (args[0].toLowerCase().equals("stop") ) {
-
-				if (Announcements.stopAnnouncement(args[1].toLowerCase()) == true) {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_stopped", args[1].toUpperCase());
-				} else {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_stopped_error", args[1].toUpperCase());
-				}
-
-			} else {
-
-				showCommandUsage(sender);
-			}
-
-		} else if (args.length == 3) {
-
-			if (isInteger(args[2])) {
-
-				if (args[0].toLowerCase().equals("start")) {
-
-					if (Announcements.startAnnouncement(args[1].toLowerCase(), Integer.parseInt(args[2])) == true) {
-						MessageManager.sendSpecialMessage(sender, "command_announcement_started", args[1].toUpperCase());
-					} else {
-						MessageManager.sendSpecialMessage(sender, "command_announcement_started_error", args[1].toUpperCase());
-					}
-
-				} else {
-
-					showCommandUsage(sender);
-
-				}
-
-			} else if (args[0].toLowerCase().equals("add")) {
-
-				if (Announcements.addAnnouncement(args[1].toLowerCase(), args[2]) == true) {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_added", args[1].toUpperCase());
-				} else {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_added_error", args[1].toUpperCase());
-				}
-
-			} else {
-
-				showCommandUsage(sender);
-
-			}
-
-		} else if (args.length >= 3) {
-
-			if (args[0].toLowerCase().equals("add")) {
-
-				String message = MultiChatUtil.getMessageFromArgs(args, 2);
-
-				if (Announcements.addAnnouncement(args[1].toLowerCase(), message) == true) {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_added", args[1].toUpperCase());
-				} else {
-					MessageManager.sendSpecialMessage(sender, "command_announcement_added_error", args[1].toUpperCase());
-				}
-
-			} else {
-
-				showCommandUsage(sender);
-
-			}
-
-		} else {
-
-			showCommandUsage(sender);
-
+    public void execute(CommandSender sender, String[] args) {
+    	if (args.length == 0) {
+    		showCommandUsage(sender);
+    		return;
 		}
 
-	}
+    	String arg = args[0].toLowerCase();
+    	switch (arg) {
+			case "list": {
+				MessageManager.sendMessage(sender, "command_announcement_list");
+				Announcements.getAnnouncementList().forEach((key, value) ->
+						MessageManager.sendSpecialMessage(sender,
+								"command_announcement_list_item",
+								key + ": +++" + value,
+								true
+						)
+				);
+				break;
+			}
+			case "add": {
+				if (args.length < 3) {
+					showCommandUsage(sender);
+					return;
+				}
 
-	public static boolean isInteger(String str) { 
+				String announcementKey = args[1].toLowerCase();
+				String message = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+				if (Announcements.addAnnouncement(announcementKey, message)) {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_added", announcementKey);
+				} else {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_added_error", announcementKey);
+				}
+				break;
+			}
+			case "remove": {
+				if (args.length < 2) {
+					showCommandUsage(sender);
+					return;
+				}
 
-		try {  
+				String announcementKey = args[1].toLowerCase();
+				if (Announcements.removeAnnouncement(announcementKey)) {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_removed", announcementKey);
+				} else {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_does_not_exist", announcementKey);
+				}
+				break;
+			}
+			case "start": {
+				int timer;
+				if (args.length < 3 || (timer = parseInt(args[2])) == -1) {
+					showCommandUsage(sender);
+					return;
+				}
 
-			@SuppressWarnings("unused")
-			int n = Integer.parseInt(str);  
+				String announcementKey = args[1].toLowerCase();
+				if (Announcements.startAnnouncement(announcementKey, timer)) {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_started", announcementKey);
+				} else {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_started_error", announcementKey);
+				}
+				break;
+			}
+			case "stop": {
+				if (args.length < 2) {
+					showCommandUsage(sender);
+					return;
+				}
 
-		} catch(NumberFormatException nfe) {  
-			return false;  
-		}  
+				String announcementKey = args[1].toLowerCase();
+				if (Announcements.stopAnnouncement(announcementKey)) {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_stopped", announcementKey);
+				} else {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_stopped_error", announcementKey);
+				}
+				break;
+			}
+			default: {
+				if (Announcements.existsAnnouncemnt(arg)) {
+					Announcements.playAnnouncement(arg);
+				} else {
+					MessageManager.sendSpecialMessage(sender, "command_announcement_does_not_exist", arg);
+				}
+				break;
+			}
+		}
+    }
 
-		return true;  
-	}
+    private int parseInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException nfe) {
+            return -1;
+        }
+    }
 
-	private void showCommandUsage(CommandSender sender) {
-
-		MessageManager.sendMessage(sender, "command_announcement_usage");
-		sender.sendMessage(new ComponentBuilder("/announcement add <name> <message>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/announcement remove <name>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/announcement start <name> <interval in minutes>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/announcement stop <name>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/announcement list").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/announce <name>").color(ChatColor.AQUA).create());
-
-	}
-
+    private void showCommandUsage(CommandSender sender) {
+        MessageManager.sendMessage(sender, "command_announcement_usage");
+        sender.sendMessage(new ComponentBuilder("/announcement add <name> <message>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/announcement remove <name>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/announcement start <name> <interval in minutes>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/announcement stop <name>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/announcement list").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/announce <name>").color(ChatColor.AQUA).create());
+    }
 }
