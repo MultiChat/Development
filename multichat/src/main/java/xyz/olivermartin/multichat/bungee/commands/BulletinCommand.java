f9cc7b19946a753e32577f8b741c392e69f820a2
Gadse <lilahippie@gmail.com>
2020-08-04 07:32:16 +0200

    BulletinCommand overhaul

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/BulletinCommand.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/BulletinCommand.java
@@ -1,5 +1,6 @@
 package xyz.olivermartin.multichat.bungee.commands;
 
+import java.util.Arrays;
 import java.util.Iterator;
 
 import net.md_5.bungee.api.ChatColor;
@@ -9,112 +10,101 @@ import net.md_5.bungee.api.plugin.Command;
 import xyz.olivermartin.multichat.bungee.Bulletins;
 import xyz.olivermartin.multichat.bungee.ConfigManager;
 import xyz.olivermartin.multichat.bungee.MessageManager;
-import xyz.olivermartin.multichat.common.MultiChatUtil;
 import xyz.olivermartin.multichat.proxy.common.config.ConfigFile;
 
 /**
  * Bulletin Command
  * <p>Allows the user to create, start and stop bulletins</p>
- * 
- * @author Oliver Martin (Revilo410)
  *
+ * @author Oliver Martin (Revilo410)
  */
 public class BulletinCommand extends Command {
 
-	public BulletinCommand() {
-		super("mcbulletin", "multichat.bulletin", (String[]) ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("bulletin").toArray(new String[0]));
-	}
+    public BulletinCommand() {
+        super("mcbulletin", "multichat.bulletin", ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("bulletin").toArray(new String[0]));
+    }
 
-	@Override
-	public void execute(CommandSender sender, String[] args) {
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length == 0) {
+            showCommandUsage(sender);
+            return;
+        }
 
-		if (args.length < 1) {
+        String arg = args[0].toLowerCase();
+        switch (arg) {
+            case "stop": {
+                Bulletins.stopBulletins();
+                MessageManager.sendMessage(sender, "command_bulletin_stopped");
+                break;
+            }
+            case "list": {
+                // TODO: Refactor Bulletins to change this part properly
+                int counter = 0;
+                Iterator<String> it = Bulletins.getIterator();
 
-			showCommandUsage(sender);
+                MessageManager.sendMessage(sender, "command_bulletin_list");
+                while (it.hasNext()) {
+                    counter++;
+                    MessageManager.sendSpecialMessage(sender, "command_bulletin_list_item", counter + ": +++" + it.next(), true);
+                }
+                break;
+            }
+            case "add": {
+                if (args.length < 2) {
+                    showCommandUsage(sender);
+                    return;
+                }
 
-		} else if (args.length == 1) {
+                Bulletins.addBulletin(String.join(" ", Arrays.copyOfRange(args, 1, args.length)));
+                MessageManager.sendMessage(sender, "command_bulletin_added");
+                break;
+            }
+            case "remove": {
+                if (args.length < 2) {
+                    showCommandUsage(sender);
+                    return;
+                }
 
-			if (args[0].toLowerCase().equals("stop")) {
+                int id;
+                try {
+                    id = Integer.parseInt(args[1]);
+                } catch (NumberFormatException ignored) {
+                    MessageManager.sendMessage(sender, "command_bulletin_invalid_usage");
+                    return;
+                }
 
-				Bulletins.stopBulletins();
-				MessageManager.sendMessage(sender, "command_bulletin_stopped");
+                Bulletins.removeBulletin(id - 1);
+                MessageManager.sendMessage(sender, "command_bulletin_removed");
+                break;
+            }
+            case "start": {
+                if (args.length < 2) {
+                    showCommandUsage(sender);
+                    return;
+                }
 
-			} else if (args[0].toLowerCase().equals("list")) {
+                int bulletinDelay;
+                try {
+                    bulletinDelay = Integer.parseInt(args[1]);
+                } catch (NumberFormatException ignored) {
+                    MessageManager.sendMessage(sender, "command_bulletin_invalid_usage");
+                    return;
+                }
 
-				int counter = 0;
-				Iterator<String> it = Bulletins.getIterator();
+                Bulletins.startBulletins(bulletinDelay);
+                MessageManager.sendMessage(sender, "command_bulletin_started");
+                break;
+            }
+        }
+    }
 
-				MessageManager.sendMessage(sender, "command_bulletin_list");
-				while (it.hasNext()) {
-					counter++;
-					MessageManager.sendSpecialMessage(sender, "command_bulletin_list_item", counter + ": +++" + it.next(), true);
-				}
-
-			} else {
-
-				showCommandUsage(sender);
-
-			}
-
-		} else if (args.length == 2) {
-
-			if (args[0].toLowerCase().equals("remove")) {
-
-				try {
-
-					Bulletins.removeBulletin(Integer.parseInt(args[1]) - 1);
-					MessageManager.sendMessage(sender, "command_bulletin_removed");
-
-				} catch (Exception e) {
-					MessageManager.sendMessage(sender, "command_bulletin_invalid_usage");
-				}
-
-			} else if (args[0].toLowerCase().equals("start") ) {
-
-				try {
-					Bulletins.startBulletins(Integer.parseInt(args[1]));
-					MessageManager.sendMessage(sender, "command_bulletin_started");
-				} catch (Exception e) {
-					MessageManager.sendMessage(sender, "command_bulletin_invalid_usage");
-				}
-
-			} else if (args[0].toLowerCase().equals("add") ) {
-
-				Bulletins.addBulletin(args[1]);
-				MessageManager.sendMessage(sender, "command_bulletin_added");
-
-			} else {
-
-				showCommandUsage(sender);
-
-			}
-
-		} else if (args.length > 2) {
-
-			if (args[0].toLowerCase().equals("add")) {
-
-				String message = MultiChatUtil.getMessageFromArgs(args, 1);
-
-				Bulletins.addBulletin(message);
-				MessageManager.sendMessage(sender, "command_bulletin_added");
-			}
-
-		} else {
-
-			showCommandUsage(sender);
-
-		}
-
-	}
-
-	private void showCommandUsage(CommandSender sender) {
-
-		MessageManager.sendMessage(sender, "command_bulletin_usage");
-		sender.sendMessage(new ComponentBuilder("/bulletin add <message>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/bulletin remove <index>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/bulletin start <interval in minutes>").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/bulletin stop").color(ChatColor.AQUA).create());
-		sender.sendMessage(new ComponentBuilder("/bulletin list").color(ChatColor.AQUA).create());
-
-	}
+    private void showCommandUsage(CommandSender sender) {
+        MessageManager.sendMessage(sender, "command_bulletin_usage");
+        sender.sendMessage(new ComponentBuilder("/bulletin add <message>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/bulletin remove <index>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/bulletin start <interval in minutes>").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/bulletin stop").color(ChatColor.AQUA).create());
+        sender.sendMessage(new ComponentBuilder("/bulletin list").color(ChatColor.AQUA).create());
+    }
 }
