299fc1bbd972e258c25da4eeee260bcf7b8220ec
Gadse <lilahippie@gmail.com>
2020-08-05 21:35:09 +0200

    HelpMeCommand refactor

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/HelpMeCommand.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/HelpMeCommand.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+import org.apache.commons.lang.StringUtils;
 import xyz.olivermartin.multichat.bungee.ChatControl;
 import xyz.olivermartin.multichat.bungee.ConfigManager;
 import xyz.olivermartin.multichat.bungee.ConsoleManager;
@@ -16,73 +17,51 @@ import xyz.olivermartin.multichat.proxy.common.config.ConfigFile;
 /**
  * 'Help Me' Command
  * <p>Allows players to request help from all online staff members</p>
- * 
- * @author Oliver Martin (Revilo410)
  *
+ * @author Oliver Martin (Revilo410)
  */
 public class HelpMeCommand extends Command {
 
-	public HelpMeCommand() {
-		super("mchelpme", "multichat.chat.helpme", (String[]) ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("helpme").toArray(new String[0]));
-	}
+    public HelpMeCommand() {
+        super("mchelpme", "multichat.chat.helpme", ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("helpme").toArray(new String[0]));
+    }
 
-	public void execute(CommandSender sender, String[] args) {
+    public void execute(CommandSender sender, String[] args) {
+        if (!(sender instanceof ProxiedPlayer)) {
+            MessageManager.sendMessage(sender, "command_helpme_only_players");
+            return;
+        }
 
-		if ( sender instanceof ProxiedPlayer ) {
+        if (args.length < 1) {
+            MessageManager.sendMessage(sender, "command_helpme_desc");
+            MessageManager.sendMessage(sender, "command_helpme_usage");
+            return;
+        }
 
-			if (args.length < 1) {
+        ProxiedPlayer proxiedPlayer = (ProxiedPlayer) sender;
 
-				MessageManager.sendMessage(sender, "command_helpme_desc");
-				MessageManager.sendMessage(sender, "command_helpme_usage");
+        if (ChatControl.isMuted(proxiedPlayer.getUniqueId(), "helpme")) {
+            MessageManager.sendMessage(proxiedPlayer, "mute_cannot_send_message");
+            return;
+        }
 
-			} else { 
+        String message = String.join(" ", args);
 
-				String message = MultiChatUtil.getMessageFromArgs(args);
+        if (ChatControl.handleSpam(proxiedPlayer, message, "helpme")) {
+            return;
+        }
 
-				if ( sendMessage(sender.getName() + ": " + message, sender.getName()) ) {
-					MessageManager.sendMessage(sender, "command_helpme_sent");
-				}
+        Optional<String> crm = ChatControl.applyChatRules(message, "helpme", proxiedPlayer.getName());
+        if (!crm.isPresent())
+            return;
 
-			}
+        String finalMessage = crm.get();
 
-		} else {
-			MessageManager.sendMessage(sender, "command_helpme_only_players");
-		}
-	}
+        ProxyServer.getInstance().getPlayers().stream()
+                .filter(target -> target.hasPermission("multichat.staff"))
+                .forEach(target -> MessageManager.sendSpecialMessage(target, "command_helpme_format", finalMessage));
 
-	public static boolean sendMessage(String message, String username) {
-
-		Optional<String> crm;
-
-		ProxiedPlayer potentialPlayer = ProxyServer.getInstance().getPlayer(username);
-		if (potentialPlayer != null) {
-			if (ChatControl.isMuted(potentialPlayer.getUniqueId(), "helpme")) {
-				MessageManager.sendMessage(potentialPlayer, "mute_cannot_send_message");
-				return false;
-			}
-
-			if (ChatControl.handleSpam(potentialPlayer, message, "helpme")) {
-				return false;
-			}
-		}
-
-		crm = ChatControl.applyChatRules(message, "helpme", username);
-
-		if (crm.isPresent()) {
-			message = crm.get();
-		} else {
-			return false;
-		}
-
-		for (ProxiedPlayer onlineplayer : ProxyServer.getInstance().getPlayers()) {
-			if (onlineplayer.hasPermission("multichat.staff")) {
-				MessageManager.sendSpecialMessage(onlineplayer, "command_helpme_format", message);
-			}
-		}
-
-		ConsoleManager.logHelpMe(message);
-
-		return true;
-
-	}
+        ConsoleManager.logHelpMe(message);
+        MessageManager.sendMessage(sender, "command_helpme_sent");
+    }
 }
