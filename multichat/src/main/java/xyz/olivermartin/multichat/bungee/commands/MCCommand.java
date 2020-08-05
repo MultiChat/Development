6f3889fc203147863c440851a6559c4bce77fe97
Gadse <lilahippie@gmail.com>
2020-08-05 22:54:29 +0200

    MCCommand refactor

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/MCCommand.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/MCCommand.java
@@ -6,61 +6,47 @@ import net.md_5.bungee.api.plugin.Command;
 import xyz.olivermartin.multichat.bungee.ConfigManager;
 import xyz.olivermartin.multichat.bungee.Events;
 import xyz.olivermartin.multichat.bungee.MessageManager;
-import xyz.olivermartin.multichat.common.MultiChatUtil;
 import xyz.olivermartin.multichat.proxy.common.config.ConfigFile;
 import xyz.olivermartin.multichat.bungee.StaffChatManager;
+
+import java.util.UUID;
 
 /**
  * Mod-Chat Commands
  * <p>Allows staff members to send mod-chat messages or toggle the chat</p>
- * 
- * @author Oliver Martin (Revilo410)
  *
+ * @author Oliver Martin (Revilo410)
  */
 public class MCCommand extends Command {
 
-	public MCCommand() {
-		super("mcmc", "multichat.staff.mod", (String[]) ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("mc").toArray(new String[0]));
-	}
+    public MCCommand() {
+        super("mcmc", "multichat.staff.mod", ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("mc").toArray(new String[0]));
+    }
 
-	public void execute(CommandSender sender, String[] args) {
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length == 0) {
+            if (!(sender instanceof ProxiedPlayer)) {
+                MessageManager.sendMessage(sender, "command_mc_only_players");
+                return;
+            }
 
-		boolean toggleresult;
+            UUID playerUID = ((ProxiedPlayer) sender).getUniqueId();
+            boolean toggleResult = Events.toggleMC(playerUID);
+            MessageManager.sendMessage(sender, "command_mc_toggle_" + (toggleResult ? "on" : "off"));
+            return;
+        }
 
-		if (args.length < 1) {
+        String name = "CONSOLE";
+        String displayName = "CONSOLE";
+        String serverName = "#";
 
-			if ((sender instanceof ProxiedPlayer)) {
+        if (sender instanceof ProxiedPlayer) {
+            ProxiedPlayer proxiedPlayer = (ProxiedPlayer) sender;
+            name = proxiedPlayer.getName();
+            displayName = proxiedPlayer.getDisplayName();
+            serverName = proxiedPlayer.getServer().getInfo().getName();
+        }
 
-				ProxiedPlayer player = (ProxiedPlayer) sender;
-				toggleresult = Events.toggleMC(player.getUniqueId());
-
-				if (toggleresult == true) {
-					MessageManager.sendMessage(sender, "command_mc_toggle_on");
-				} else {
-					MessageManager.sendMessage(sender, "command_mc_toggle_off");
-				}
-
-			} else {
-				MessageManager.sendMessage(sender, "command_mc_only_players");
-			}
-
-		} else if ((sender instanceof ProxiedPlayer)) {
-
-			String message = MultiChatUtil.getMessageFromArgs(args);
-
-			ProxiedPlayer player = (ProxiedPlayer) sender;
-
-			StaffChatManager chatman = new StaffChatManager();
-			chatman.sendModMessage(player.getName(), player.getDisplayName(), player.getServer().getInfo().getName(), message);
-			chatman = null;
-
-		} else {
-
-			String message = MultiChatUtil.getMessageFromArgs(args);
-
-			StaffChatManager chatman = new StaffChatManager();
-			chatman.sendModMessage("CONSOLE", "CONSOLE", "#", message);
-			chatman = null;
-		}
-	}
+        new StaffChatManager().sendModMessage(name, displayName, serverName, String.join(" ", args));
+    }
 }
