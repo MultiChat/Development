1c993945ebf83b0e89ef731a59a194c4962b86f9
Gadse <lilahippie@gmail.com>
2020-08-06 14:38:48 +0200

    MuteCommand refactor

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/MuteCommand.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/commands/MuteCommand.java
@@ -11,50 +11,37 @@ import xyz.olivermartin.multichat.proxy.common.config.ConfigFile;
 
 public class MuteCommand extends Command {
 
-	public MuteCommand() {
-		super("mcmute", "multichat.mute", (String[])ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("mute").toArray(new String[0]));
-	}
+    public MuteCommand() {
+        super("mcmute", "multichat.mute", ConfigManager.getInstance().getHandler(ConfigFile.ALIASES).getConfig().getStringList("mute").toArray(new String[0]));
+    }
 
-	@Override
-	public void execute(CommandSender sender, String[] args) {
-		
-		if (!ConfigManager.getInstance().getHandler(ConfigFile.CHAT_CONTROL).getConfig().getBoolean("mute")) return;
-		
-		if (args.length != 1) {
-			
-			MessageManager.sendMessage(sender, "mute_usage");
-			
-		} else {
-			
-			String username = args[0];
-			
-			ProxiedPlayer target = ProxyServer.getInstance().getPlayer(username);
-			
-			if (target != null) {
-				
-				if (target.hasPermission("multichat.mute.bypass")) {
-					MessageManager.sendMessage(sender, "mute_bypass");
-					return;
-				}
-				
-				if (!ChatControl.isMutedAnywhere(target.getUniqueId())) {
-					ChatControl.mute(target.getUniqueId());
-					MessageManager.sendMessage(sender, "mute_muted_staff");
-					MessageManager.sendMessage(target, "mute_muted");
-				} else {
-					ChatControl.unmute(target.getUniqueId());
-					MessageManager.sendMessage(sender, "mute_unmuted_staff");
-					MessageManager.sendMessage(target, "mute_unmuted");
-				}
-				
-			} else {
-				
-				MessageManager.sendMessage(sender, "mute_player_not_found");
-				
-			}
-			
-		}
-		
-	}
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+        if (args.length < 1) {
+            MessageManager.sendMessage(sender, "mute_usage");
+            return;
+        }
 
+        ProxiedPlayer target = ProxyServer.getInstance().getPlayer(args[0]);
+
+        if (target == null) {
+            MessageManager.sendMessage(sender, "mute_player_not_found");
+            return;
+        }
+
+        if (target.hasPermission("multichat.mute.bypass")) {
+            MessageManager.sendMessage(sender, "mute_bypass");
+            return;
+        }
+
+        if (!ChatControl.isMutedAnywhere(target.getUniqueId())) {
+            ChatControl.mute(target.getUniqueId());
+            MessageManager.sendMessage(sender, "mute_muted_staff");
+            MessageManager.sendMessage(target, "mute_muted");
+        } else {
+            ChatControl.unmute(target.getUniqueId());
+            MessageManager.sendMessage(sender, "mute_unmuted_staff");
+            MessageManager.sendMessage(target, "mute_unmuted");
+        }
+    }
 }
