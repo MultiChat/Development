dfcfe1c3901fab81f9f2dc386c62407f7588c601
Gadse <lilahippie@gmail.com>
2020-08-08 09:05:49 +0200

    MultiChat.class optimizations

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/MultiChat.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/MultiChat.java
@@ -28,6 +28,8 @@ import xyz.olivermartin.multichat.proxy.common.storage.files.*;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 
@@ -41,8 +43,7 @@ public class MultiChat extends Plugin {
 
     public static final String LATEST_VERSION = "1.10";
 
-    public static final String[] ALLOWED_VERSIONS = new String[]{
-
+    public static final Set<String> ALLOWED_VERSIONS = new HashSet<>(Arrays.asList(
             LATEST_VERSION,
             "1.9.5",
             "1.9.4",
@@ -73,26 +74,21 @@ public class MultiChat extends Plugin {
             "1.3.2",
             "1.3.1",
             "1.3"
-
-    };
+    ));
 
     // Config values
     public static boolean premiumVanish = false;
 
     public void fetchDisplayNames() {
 
-        getProxy().getScheduler().schedule(this, new Runnable() {
+        getProxy().getScheduler().schedule(this, () -> {
 
-            public void run() {
+            if (ProxyConfigs.CONFIG.isFetchSpigotDisplayNames()) {
 
-                if (ProxyConfigs.CONFIG.isFetchSpigotDisplayNames()) {
-
-                    for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
-                        if (player.getServer() != null) {
-                            ProxyLocalCommunicationManager.sendUpdatePlayerMetaRequestMessage(player.getName(), player.getServer().getInfo());
-                        }
+                for (ProxiedPlayer player : ProxyServer.getInstance().getPlayers()) {
+                    if (player.getServer() != null) {
+                        ProxyLocalCommunicationManager.sendUpdatePlayerMetaRequestMessage(player.getName(), player.getServer().getInfo());
                     }
-
                 }
 
             }
@@ -116,165 +112,160 @@ public class MultiChat extends Plugin {
 
         File configDirectory = getDataFolder();
         if (!getDataFolder().exists()) {
-            System.out.println("[MultiChat] Creating plugin directory!");
-            getDataFolder().mkdirs();
+            getLogger().info("Creating plugin directory");
+            if (!getDataFolder().mkdirs()) {
+                getLogger().severe("Could not create plugin directory. Check write access to the plugins folder!");
+                return;
+            }
         }
         MultiChatProxy.getInstance().registerConfigDirectory(configDirectory);
 
         File translationsDir = new File(configDirectory.toString() + File.separator + "translations");
         if (!translationsDir.exists()) {
-            System.out.println("[MultiChat] Creating translations directory!");
-            translationsDir.mkdirs();
+            getLogger().info("Creating translations directory");
+            if (!translationsDir.mkdirs()) {
+                getLogger().severe("Could not create translations directory. Check write access to the plugins folder!");
+                return;
+            }
         }
 
-        ProxyConfigs.ALL.forEach(abstractProxyConfig -> abstractProxyConfig.reloadConfig(this));
+        ProxyConfigs.ALL.forEach(proxyConfig -> {
+            proxyConfig.reloadConfig(this);
+            ProxyConfigs.loadRawConfig(this, proxyConfig.getFileName().replace(".yml", "_fr.yml"), translationsDir);
+        });
 
-        // TODO: [ConfigRefactor] Maybe have a list somewhere through which we can simply loop and call #loadRawConfig
-        //  for future translations
-        ProxyConfigs.loadRawConfig(this, "messages_fr.yml", translationsDir);
-        ProxyConfigs.loadRawConfig(this, "joinmessages_fr.yml", translationsDir);
-        ProxyConfigs.loadRawConfig(this, "config_fr.yml", translationsDir);
-        ProxyConfigs.loadRawConfig(this, "chatcontrol_fr.yml", translationsDir);
-        ProxyConfigs.loadRawConfig(this, "aliases_fr.yml", translationsDir);
 
-        if (Arrays.asList(ALLOWED_VERSIONS).contains(ProxyConfigs.CONFIG.getVersion())) {
-
-            if (!ProxyConfigs.CONFIG.getVersion().equals(LATEST_VERSION)) {
-                getLogger().warning("YOUR CONFIG FILES ARE NOT THE LATEST VERSION");
-                getLogger().warning("SOME FEATURES OF MULTICHAT ARE ONLY PRESENT IN THE LATEST VERSION OF THE CONFIG");
-            }
-
-            // Register listeners
-            getProxy().getPluginManager().registerListener(this, new Events());
-
-            // New listeners (1.10+)
-            getProxy().getPluginManager().registerListener(this, new ProxyLoginListener());
-            getProxy().getPluginManager().registerListener(this, new ProxyServerConnectedListener());
-            getProxy().getPluginManager().registerListener(this, new ProxyLogoutListener());
-            getProxy().getPluginManager().registerListener(this, new ProxyServerSwitchListener());
-
-            // Communication Channels
-            getProxy().registerChannel(CommChannels.PLAYER_META); // pmeta
-            getProxy().registerChannel(CommChannels.PLAYER_CHAT); // pchat
-            getProxy().registerChannel(CommChannels.SERVER_CHAT); // schat
-            getProxy().registerChannel(CommChannels.PLAYER_ACTION); // pact
-            getProxy().registerChannel(CommChannels.SERVER_ACTION); // sact
-            getProxy().registerChannel(CommChannels.PLAYER_DATA); // pdata
-            getProxy().registerChannel(CommChannels.SERVER_DATA); // sdata
-            getProxy().getPluginManager().registerListener(this, new ProxyPlayerMetaListener()); // list - pmeta
-            getProxy().getPluginManager().registerListener(this, new ProxyPlayerChatListener()); // list - pchat
-            getProxy().getPluginManager().registerListener(this, new ProxyPlayerActionListener()); // list - pact
-            getProxy().getPluginManager().registerListener(this, new ProxyServerActionListener()); // list - sact
-
-            // Register commands
-            registerCommands();
-
-            // TODO: Change to appropriate logger
-            System.out.println("[MultiChat] Config Version: " + ProxyConfigs.CONFIG.getVersion());
-
-            // Run start-up routines
-            ProxyFileStoreManager fileStoreManager = new ProxyFileStoreManager();
-
-            fileStoreManager.registerFileStore("announcements.dat",
-                    new ProxyAnnouncementsFileStore("Announcements.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("bulletins.dat",
-                    new ProxyBulletinsFileStore("Bulletins.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("staffchatinfo.dat",
-                    new ProxyStaffChatFileStore("StaffChatInfo.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("adminchatinfo.dat",
-                    new ProxyAdminChatFileStore("AdminChatInfo.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("groupchatinfo.dat",
-                    new ProxyGroupChatFileStore("GroupChatInfo.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("groupspyinfo.dat",
-                    new ProxyGroupSpyFileStore("GroupSpyInfo.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("casts.dat",
-                    new ProxyCastsFileStore("Casts.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("socialspyinfo.dat",
-                    new ProxySocialSpyFileStore("SocialSpyInfo.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("globalchatinfo.dat",
-                    new ProxyGlobalChatFileStore("GlobalChatInfo.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("mute.dat",
-                    new ProxyMuteFileStore("Mute.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("ignore.dat",
-                    new ProxyIgnoreFileStore("Ignore.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("multichatuuidname.dat",
-                    new ProxyUUIDNameFileStore("MultiChatUUIDName.dat", configDirectory));
-
-            fileStoreManager.registerFileStore("localspyinfo.dat",
-                    new ProxyLocalSpyFileStore("LocalSpyInfo.dat", configDirectory));
-
-            MultiChatProxy.getInstance().registerFileStoreManager(fileStoreManager);
-
-            // Set default channel
-            String defaultChannel = ProxyConfigs.CONFIG.getDefaultChannel();
-            boolean forceChannelOnJoin = ProxyConfigs.CONFIG.isForceChannelOnJoin();
-
-            // New context manager and channels
-            GlobalContext globalContext = new GlobalContext(defaultChannel, forceChannelOnJoin, true);
-            ContextManager contextManager = new ContextManager(globalContext);
-            MultiChatProxy.getInstance().registerContextManager(contextManager);
-
-            ChannelManager channelManager = new ChannelManager();
-            channelManager.setGlobalChannel(new GlobalStaticProxyChannel("Global Channel",
-                    ProxyConfigs.CONFIG.getGlobalFormat(),
-                    channelManager)
-            );
-            channelManager.setLocalChannel(new LocalChannel("Local Channel",
-                    ProxyConfigs.CONFIG.getGlobalFormat(),
-                    channelManager)
-            );
-            MultiChatProxy.getInstance().registerChannelManager(channelManager);
-
-            // TODO This is just a test channel
-            channelManager.registerProxyChannel(new StaticProxyChannel("test", new ProxyChannelInfo("A test channel", "&8[&7TEST&8] &f%DISPLAYNAME%&f: ", false, globalContext, "multichat.chat.channel.test", "multichat.chat.channel.test.view"), channelManager));
-
-            ProxyChatManager chatManager = new ProxyChatManager();
-            MultiChatProxy.getInstance().registerChatManager(chatManager);
-
-            MultiChatProxy.getInstance().registerTagManager(new TagManager());
-
-            ///
-
-            // Initiate backup routine
-            ProxyBackupManager backupManager = new ProxyBackupManager();
-            MultiChatProxy.getInstance().registerBackupManager(backupManager);
-            backupManager.registerBackupTask(new Runnable() {
-
-                public void run() {
-                    MultiChatProxy.getInstance().getFileStoreManager().save();
-                }
-
-            });
-            backupManager.startBackup(1L, 60L, TimeUnit.MINUTES);
-
-            // Fetch display names of all players
-            fetchDisplayNames();
-
-            // Manage premiumVanish dependency
-            if (ProxyServer.getInstance().getPluginManager().getPlugin("PremiumVanish") != null) {
-                premiumVanish = true;
-                System.out.println("[MultiChat] Hooked with PremiumVanish!");
-            }
-
-        } else {
+        if (!ALLOWED_VERSIONS.contains(ProxyConfigs.CONFIG.getVersion())) {
             getLogger().info("Config incorrect version! Please repair or delete it!");
+            return;
+        }
+
+        if (!ProxyConfigs.CONFIG.getVersion().equals(LATEST_VERSION)) {
+            getLogger().warning("YOUR CONFIG FILES ARE NOT THE LATEST VERSION");
+            getLogger().warning("SOME FEATURES OF MULTICHAT ARE ONLY PRESENT IN THE LATEST VERSION OF THE CONFIG");
+        }
+
+        // Register listeners
+        getProxy().getPluginManager().registerListener(this, new Events());
+
+        // New listeners (1.10+)
+        getProxy().getPluginManager().registerListener(this, new ProxyLoginListener());
+        getProxy().getPluginManager().registerListener(this, new ProxyServerConnectedListener());
+        getProxy().getPluginManager().registerListener(this, new ProxyLogoutListener());
+        getProxy().getPluginManager().registerListener(this, new ProxyServerSwitchListener());
+
+        // Communication Channels
+        getProxy().registerChannel(CommChannels.PLAYER_META); // pmeta
+        getProxy().registerChannel(CommChannels.PLAYER_CHAT); // pchat
+        getProxy().registerChannel(CommChannels.SERVER_CHAT); // schat
+        getProxy().registerChannel(CommChannels.PLAYER_ACTION); // pact
+        getProxy().registerChannel(CommChannels.SERVER_ACTION); // sact
+        getProxy().registerChannel(CommChannels.PLAYER_DATA); // pdata
+        getProxy().registerChannel(CommChannels.SERVER_DATA); // sdata
+        getProxy().getPluginManager().registerListener(this, new ProxyPlayerMetaListener()); // list - pmeta
+        getProxy().getPluginManager().registerListener(this, new ProxyPlayerChatListener()); // list - pchat
+        getProxy().getPluginManager().registerListener(this, new ProxyPlayerActionListener()); // list - pact
+        getProxy().getPluginManager().registerListener(this, new ProxyServerActionListener()); // list - sact
+
+        // Register commands
+        registerCommands();
+
+        // TODO: Change to appropriate logger
+        getLogger().info("Config Version: " + ProxyConfigs.CONFIG.getVersion());
+
+        // Run start-up routines
+        ProxyFileStoreManager fileStoreManager = new ProxyFileStoreManager();
+
+        fileStoreManager.registerFileStore("announcements.dat",
+                new ProxyAnnouncementsFileStore("Announcements.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("bulletins.dat",
+                new ProxyBulletinsFileStore("Bulletins.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("staffchatinfo.dat",
+                new ProxyStaffChatFileStore("StaffChatInfo.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("adminchatinfo.dat",
+                new ProxyAdminChatFileStore("AdminChatInfo.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("groupchatinfo.dat",
+                new ProxyGroupChatFileStore("GroupChatInfo.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("groupspyinfo.dat",
+                new ProxyGroupSpyFileStore("GroupSpyInfo.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("casts.dat",
+                new ProxyCastsFileStore("Casts.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("socialspyinfo.dat",
+                new ProxySocialSpyFileStore("SocialSpyInfo.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("globalchatinfo.dat",
+                new ProxyGlobalChatFileStore("GlobalChatInfo.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("mute.dat",
+                new ProxyMuteFileStore("Mute.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("ignore.dat",
+                new ProxyIgnoreFileStore("Ignore.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("multichatuuidname.dat",
+                new ProxyUUIDNameFileStore("MultiChatUUIDName.dat", configDirectory));
+
+        fileStoreManager.registerFileStore("localspyinfo.dat",
+                new ProxyLocalSpyFileStore("LocalSpyInfo.dat", configDirectory));
+
+        MultiChatProxy.getInstance().registerFileStoreManager(fileStoreManager);
+
+        // Set default channel
+        String defaultChannel = ProxyConfigs.CONFIG.getDefaultChannel();
+        boolean forceChannelOnJoin = ProxyConfigs.CONFIG.isForceChannelOnJoin();
+
+        // New context manager and channels
+        GlobalContext globalContext = new GlobalContext(defaultChannel, forceChannelOnJoin, true);
+        ContextManager contextManager = new ContextManager(globalContext);
+        MultiChatProxy.getInstance().registerContextManager(contextManager);
+
+        ChannelManager channelManager = new ChannelManager();
+        channelManager.setGlobalChannel(new GlobalStaticProxyChannel("Global Channel",
+                ProxyConfigs.CONFIG.getGlobalFormat(),
+                channelManager)
+        );
+        channelManager.setLocalChannel(new LocalChannel("Local Channel",
+                ProxyConfigs.CONFIG.getGlobalFormat(),
+                channelManager)
+        );
+        MultiChatProxy.getInstance().registerChannelManager(channelManager);
+
+        // TODO This is just a test channel
+        channelManager.registerProxyChannel(new StaticProxyChannel("test", new ProxyChannelInfo("A test channel", "&8[&7TEST&8] &f%DISPLAYNAME%&f: ", false, globalContext, "multichat.chat.channel.test", "multichat.chat.channel.test.view"), channelManager));
+
+        ProxyChatManager chatManager = new ProxyChatManager();
+        MultiChatProxy.getInstance().registerChatManager(chatManager);
+
+        MultiChatProxy.getInstance().registerTagManager(new TagManager());
+
+        ///
+
+        // Initiate backup routine
+        ProxyBackupManager backupManager = new ProxyBackupManager();
+        MultiChatProxy.getInstance().registerBackupManager(backupManager);
+        backupManager.registerBackupTask(() -> MultiChatProxy.getInstance().getFileStoreManager().save());
+        backupManager.startBackup(1L, 60L, TimeUnit.MINUTES);
+
+        // Fetch display names of all players
+        fetchDisplayNames();
+
+        // Manage premiumVanish dependency
+        if (ProxyServer.getInstance().getPluginManager().getPlugin("PremiumVanish") != null) {
+            premiumVanish = true;
+            getLogger().info("Hooked with PremiumVanish!");
         }
     }
 
     public void onDisable() {
 
-        getLogger().info("Thankyou for using MultiChat. Disabling...");
+        getLogger().info("Thank you for using MultiChat. Disabling...");
 
         MultiChatProxy.getInstance().getFileStoreManager().save();
 
