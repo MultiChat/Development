a8c16d68ea99501ea937ba04aedbab9f4a96535a
Deko Ration <gadse.deko@gmail.com>
2021-01-30 07:01:54 +0100

    Fix plugin message channel exploit

--- multichat/src/main/java/xyz/olivermartin/multichat/bungee/BungeeComm.java
+++ multichat/src/main/java/xyz/olivermartin/multichat/bungee/BungeeComm.java
@@ -180,19 +180,12 @@ public class BungeeComm implements Listener {
 
 	@EventHandler
 	public void onPluginMessage(PluginMessageEvent ev) {
-		if (! (ev.getTag().equals("multichat:comm") || ev.getTag().equals("multichat:chat") || ev.getTag().equals("multichat:prefix") || ev.getTag().equals("multichat:suffix") || ev.getTag().equals("multichat:dn") || ev.getTag().equals("multichat:world") || ev.getTag().equals("multichat:nick") || ev.getTag().equals("multichat:pxe") || ev.getTag().equals("multichat:ppxe")) ) {
+		if (!ev.getTag().startsWith("multichat:"))
 			return;
-		}
 
 		if (!(ev.getSender() instanceof Server)) {
+			ev.setCancelled(true);
 			return;
-		}
-
-		if (ev.getTag().equals("multichat:comm")) {
-
-			// TODO Remove - legacy
-			return;
-
 		}
 
 		if (ev.getTag().equals("multichat:chat")) {
